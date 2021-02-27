5890dd4e73a608ef14860f28234fc0a57a470ce0
popcornell <cornellsamuele@gmail.com>
2021-02-27 22:20:25 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="desed",
+    version="0.0.1",
+    description="Sound Event Detection and Separation in Domestic Environments.",
+    author="DCASE2021 Task 4 Organizers",
+    author_email="cornellsamuele@gmail.com",
+    license="MIT",
+    packages=["desed"],
+    python_requires=">=3.6",
+)
