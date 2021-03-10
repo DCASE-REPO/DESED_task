797a4873d68e3aa61d8fc37789d95f3f54981a1e
nturpaul <nicolas.turpault@inria.fr>
2021-03-10 23:50:46 +0100

    Modify conda environment, put pip at the end

--- conda_create_environment.sh
+++ conda_create_environment.sh
@@ -1,6 +1,6 @@
 conda create -y -n dcase2021 python=3.8
 source activate dcase2021
-pip install -e .
+
 conda install -y pandas h5py scipy
 conda install -y pytorch torchvision cudatoolkit=10.1 -c pytorch # for gpu install (or cpu in MAC)
 # conda install pytorch-cpu torchvision-cpu -c pytorch (cpu linux)
@@ -9,3 +9,4 @@ conda install -y ffmpeg -c conda-forge
 
 pip install -r requirements.txt 
 pip install scaper
+pip install -e .
