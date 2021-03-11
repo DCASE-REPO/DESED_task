3c915981c31143b0c084c39ea34158f37bd2067f
nturpaul <nicolas.turpault@inria.fr>
2021-03-11 16:31:33 +0100

    Fix conda environment

--- conda_create_environment.sh
+++ conda_create_environment.sh
@@ -1,12 +1,13 @@
-conda create -y -n dcase2021 python=3.8
+conda create -y -n dcase2021 python==3.8
 source activate dcase2021
 
-conda install -y pandas h5py scipy
-conda install -y pytorch torchvision cudatoolkit=10.1 -c pytorch # for gpu install (or cpu in MAC)
-# conda install pytorch-cpu torchvision-cpu -c pytorch (cpu linux)
-conda install -y pysoundfile librosa youtube-dl tqdm -c conda-forge
+conda install -y numba
+conda install -y librosa -c conda-forge
 conda install -y ffmpeg -c conda-forge
+conda install -y pandas h5py scipy
+conda install -y pytorch torchvision torchaudio cudatoolkit=10.1 -c pytorch # for gpu install (or cpu in MAC)
+# conda install pytorch-cpu torchvision-cpu -c pytorch (cpu linux)
+conda install -y youtube-dl tqdm -c conda-forge
 
-pip install -r requirements.txt 
-pip install scaper
+pip install -r requirements.txt
 pip install -e .
