0120bcbec54614c915dc74fed5fac5dd734df7fd
Francesca Ronchini <fronchini@fnancy>
2022-04-07 13:39:55 +0200

    update env dcase2022

--- recipes/dcase2022_task4_baseline/conda_create_environment.sh
+++ recipes/dcase2022_task4_baseline/conda_create_environment.sh
@@ -4,9 +4,10 @@ source activate dcase2022
 conda install -y numba
 conda install -y librosa -c conda-forge
 conda install -y ffmpeg -c conda-forge
-conda install -y conda-forge sox
+conda install -y sox -c conda-forge
 conda install -y pandas h5py scipy
-conda install -y pytorch torchvision torchaudio cudatoolkit=10.1 -c pytorch # for gpu install (or cpu in MAC)
+conda install -y pytorch-lightning==1.2.4 -c conda-forge
+conda install -y torchvision=0.9.0 torchaudio=0.8.0 cudatoolkit=10.1 -c pytorch # for gpu install (or cpu in MAC)
 # conda install pytorch-cpu torchvision-cpu -c pytorch (cpu linux)
 conda install -y youtube-dl tqdm -c conda-forge
 
