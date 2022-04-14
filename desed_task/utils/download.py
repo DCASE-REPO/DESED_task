07b827dbf25858dfff422f5178891bec06f391b5
popcornell <cornellsamuele@gmail.com>
2022-04-14 11:56:16 +0200

    added download.py script

---
+++ desed_task/utils/download.py
@@ -0,0 +1,15 @@
+from tqdm import tqdm
+import requests
+
+def download_from_url(url, destination):
+    response = requests.get(url, stream=True)
+    total_size_in_bytes = int(response.headers.get('content-length', 0))
+    block_size = 1024  # 1 Kibibyte
+    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+    with open(destination, 'wb') as file:
+        for data in response.iter_content(block_size):
+            progress_bar.update(len(data))
+            file.write(data)
+    progress_bar.close()
+    if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+        print("ERROR, something went wrong")
