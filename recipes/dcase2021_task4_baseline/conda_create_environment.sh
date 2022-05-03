cccf103689e1d15273811dd5aeeb474abe071f9f
Francesca Ronchini <fronchini@fnancy>
2022-05-03 12:19:22 +0200

    Update env 2021

--- recipes/dcase2021_task4_baseline/conda_create_environment.sh
+++ recipes/dcase2021_task4_baseline/conda_create_environment.sh
@@ -5,7 +5,7 @@ conda install -y numba
 conda install -y librosa -c conda-forge
 conda install -y ffmpeg -c conda-forge
 conda install -y pandas h5py scipy
-conda install -y pytorch torchvision torchaudio cudatoolkit=10.1 -c pytorch # for gpu install (or cpu in MAC)
+conda install -y pytorch torchvision==0.9.0 torchaudio==0.8.0 cudatoolkit=10.1 -c pytorch # for gpu install (or cpu in MAC)
 # conda install pytorch-cpu torchvision-cpu -c pytorch (cpu linux)
 conda install -y youtube-dl tqdm -c conda-forge
 
