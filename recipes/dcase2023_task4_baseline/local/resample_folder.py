f139eec474d6dc28fb78f2465e82495311f61622
Florian Angulo <florian.angulo@telecom-paris.fr>
2023-03-01 12:54:01 +0100

    Faster resampling with multiprocessing

--- recipes/dcase2023_task4_baseline/local/resample_folder.py
+++ recipes/dcase2023_task4_baseline/local/resample_folder.py
@@ -6,8 +6,9 @@ from pathlib import Path
 import librosa
 import torch
 import torchaudio
+import multiprocessing as mp
 import tqdm
-
+from tqdm.contrib.concurrent import process_map  # or thread_map
 parser = argparse.ArgumentParser("Resample a folder recursively")
 parser.add_argument(
     "--in_dir",
@@ -19,7 +20,7 @@ parser.add_argument("--target_fs", default=16000)
 parser.add_argument("--regex", type=str, default="*.wav")
 
 
-def resample(audio, orig_fs, target_fs):
+def resample(audio, orig_fs, target_fs=16000):
     """
     Resamples the audio given as input at the target_fs sample rate, if the target sample rate and the
     original sample rate are different.
@@ -36,11 +37,24 @@ def resample(audio, orig_fs, target_fs):
     for c in range(audio.shape[0]):
         tmp = audio[c].detach().cpu().numpy()
         if target_fs != orig_fs:
-            tmp = librosa.resample(tmp, orig_fs, target_fs)
+            tmp = librosa.resample(tmp, orig_sr=orig_fs, target_sr=target_fs)
         out.append(torch.from_numpy(tmp))
     out = torch.stack(out)
     return out
 
+def resample_file(tuple_args, target_fs=16000):
+    (f, in_dir, out_dir) = tuple_args
+    audio, orig_fs = torchaudio.load(f)
+    audio = resample(audio, orig_fs, target_fs)
+    os.makedirs(
+        Path(os.path.join(out_dir, Path(f).relative_to(Path(in_dir)))).parent,
+        exist_ok=True,
+    )
+    torchaudio.save(
+        os.path.join(out_dir, Path(f).relative_to(Path(in_dir))),
+        audio,
+        target_fs,
+    )
 
 def resample_folder(in_dir, out_dir, target_fs=16000, regex="*.wav"):
     """
@@ -60,21 +74,11 @@ def resample_folder(in_dir, out_dir, target_fs=16000, regex="*.wav"):
             compute = False
 
     if compute:
-        for f in tqdm.tqdm(files):
-            audio, orig_fs = torchaudio.load(f)
-            audio = resample(audio, orig_fs, target_fs)
-
-            os.makedirs(
-                Path(os.path.join(out_dir, Path(f).relative_to(Path(in_dir)))).parent,
-                exist_ok=True,
-            )
-            torchaudio.save(
-                os.path.join(out_dir, Path(f).relative_to(Path(in_dir))),
-                audio,
-                target_fs,
-            )
+        # Packing resample_file arguments to the multiprocessing pool
+        list_args = [(f, in_dir, out_dir) for f in files]
+        n_workers = min(10, mp.cpu_count())
+        process_map(resample_file, list_args, max_workers=n_workers, chunksize=1)
     return compute
-
 
 if __name__ == "__main__":
     args = parser.parse_args()
